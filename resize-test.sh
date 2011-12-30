7d5e149436c24d6dcc5db2bfbc1a526a4e14f2d6
Yongqiang Yang <xiaoqiangnk@gmail.com>
2011-12-30 08:00:55 +0800

    initial commit

---
+++ resize-test.sh
@@ -0,0 +1,95 @@
+#!/bin/sh
+
+FDISK=fdisk
+MOUNT=mount
+UMOUNT=umount
+MKFS=mkfs
+RESIZEFS=resize2fs
+feature_list="uninit_bg flex_bg bigalloc"
+extended_list="lazy_itable_init"
+
+usage()
+{
+	echo "Usage: resize-tests.sh device mountdir fstype from to"
+	exit 1
+}
+
+makefs()
+{
+	opts="-O "
+	bit=1
+	for feature in $feature_list ; do
+		if (($1 & bit)) ; then
+			opts="$opts$feature,"
+		else
+			opts="$opts^$feature,"
+		fi
+		((bit <<= 1))
+	done
+	opts="$opts -E "
+	for feature in $extended_list ; do
+		if (($1 & bit)) ;then
+			opts="$opts$feature=1,"
+		else
+			opts="$opts$feature=0,"
+		fi
+		((bit <<= 1))
+	done
+ 
+	echo $MKFS -t $FSTYP $opts $DEVICE $2
+	$MKFS -t $FSTYP $opts $DEVICE $2 &>/dev/null || exit 1
+}
+
+
+resizefs()
+{
+	echo "resizing $DEVICE from $FROM to $TO..."
+	if $RESIZEFS $DEVICE $TO &>/dev/null ; then
+		echo -e "succeeded!\n"
+	else
+		echo -e "failed!\n"
+	fi
+
+}
+
+resize_test()
+{
+	$UMOUNT $DEVICE &>/dev/null
+	makefs $1 $FROM
+	$MOUNT -o debug $DEVICE $MNTDIR
+	resizefs
+}
+
+resize_tests()
+{
+	bit=1;
+	for feature in $feature_list ; do
+		((bit <<= 1))
+	done
+	for feature in $extended_list ; do
+		((bit <<= 1))
+	done
+
+	for ((i = 0; i < bit; i++)) ; do
+		resize_test i
+	done
+}
+
+
+if [ $# -lt 5 ] ; then
+	usage
+fi
+
+DEVICE=$1
+MNTDIR=$2
+FSTYP=$3
+FROM=$4
+TO=$5
+
+case "$FSTYP" in
+    ext*)
+	 resize_tests
+	 ;;
+    *)   echo "the filesystem $FSTYP is not supported"
+	 ;;
+esac
